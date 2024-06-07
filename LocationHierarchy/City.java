cf9a178111fc7d16352032a0e030ae637785315f
gummyBearOnTheBeach <kelvin.kim@student.tdsb.on.ca>
2024-06-07 15:04:25 -0400

    Update City.java

--- LocationHierarchy/City.java
+++ LocationHierarchy/City.java
@@ -1,1 +1,54 @@
+public class City extends Location {
+  private boolean hasGym;
+  private boolean hasPokeCentre;
+  private boolean hasStore;
+
+  public City(String type, String name, int id, boolean gym, boolean pokeCenter, boolean store){
+    super(type, name, id);
+    hasGym = gym;
+    hasPokeCentre = pokeCenter;
+    hasStore = store;
+  }
+
+ public City(int id, boolean gym, boolean pokeCenter, boolean store){
+    super(id);
+    hasGym = gym;
+    hasPokeCentre = pokeCenter;
+    hasStore = store;
+ }
+
+public City(int id){
+    super(id);
+    hasGym = false;
+    hasPokeCentre = false;
+    hasStore = false;
+}
+
+
+
+      // Accessors (Getters)
+  public boolean hasGym() {
+      return hasGym;
+    }
+
+   public boolean hasPokeCentre() {
+     return hasPokeCentre;
+   }
+  public boolean hasStore() {
+     return hasStore;
+   }
+    // Mutators (Setters)
+   public void setHasGym(boolean hasGym) {
+     this.hasGym = hasGym;
+   }
+
+  public void setHasPokeCentre(boolean hasPokeCentre) {
+     this.hasPokeCentre = hasPokeCentre;
+ }
+  public void setHasStore(boolean hasStore) {
+     this.hasStore = hasStore;
+  }
+  
+}
+  
 
