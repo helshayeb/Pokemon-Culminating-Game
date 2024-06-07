d508cc996a6967a5da276f8ccccc6a088b34c82b
gummyBearOnTheBeach <kelvin.kim@student.tdsb.on.ca>
2024-06-07 15:04:51 -0400

    Update Location.java

--- LocationHierarchy/Location.java
+++ LocationHierarchy/Location.java
@@ -1,1 +1,30 @@
+public class Location{
+  private String regionType;
+  private String name;
+  private int locationID;
 
+  public Location(String type, String name, int id){
+    regionType = type;
+    this.name = name;
+    locationID = id;
+  }
+
+  public Location(int id){
+    locationID = id;
+    regionType = null
+    name = null;
+  }
+
+  public String getRegionType(){
+    return regionType;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public int getLoactionID(){
+    return locationID;
+  }
+
+}
