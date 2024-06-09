66f89077a34510677d2a6e44be263ca4b321675e
gummyBearOnTheBeach <kelvin.kim@student.tdsb.on.ca>
2024-06-08 22:35:35 -0400

    Update PokeDex.java

--- PokeDex/PokeDex.java
+++ PokeDex/PokeDex.java
@@ -1,1 +1,26 @@
+public class PokeDex {
+    double[][] typeCharrt = {{1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 2, 0.5, 1},
+            {1, 1, 0.5, 0.5, 1, 1, 2, 1, 1},
+            {1, 0, 2, 1, 2, 1, 0.5, 2, 1},
+            {1, 1, 1, 1, 0.5, 0.5, 2, 1,  1},
+            {1, 1, 1, 2, 2, 0.5, 0.5, 1, 1},
+            {1, 0.5, 0.5, 2, 0.5, 2, 0.5, 1, 1},
+            {1, 2, 1, 0, 1, 2, 0.5, 0.5, 1},
+            {1, 1, 2, 1, 1, 1, 1, 1, 0.5}};
 
+    ItemDex itDex;
+    PokemonDex pmDex;
+    LocationDex loDex;
+    MoveDex mvDex;
+
+    public PokeDex(ItemDex itDex,PokemonDex pmDex, LocationDex loDex,MoveDex mvDex){
+        this.itDex = itDex;
+        this.loDex = loDex;
+        this.mvDex = mvDex;
+        this.pmDex = pmDex;
+    }
+
+
+
+}
