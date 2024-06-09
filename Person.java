73872adc89e3b4b726b46da760115bcdec6eb62e
careless8490 <carlos.kau@student.tdsb.on.ca>
2024-06-09 17:29:29 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -45,7 +45,7 @@ public class Person {
    
    /**
    * This method is a constructor that initializes the fields of
-   * the person when given its 
+   * the person when given its name, age, and ID
    * @param n The name of the person the user inputted
    * @param a The age of the person the user inputted
    * @param i The ID of the person, retrieved from the user database
@@ -58,5 +58,99 @@ public class Person {
       currentLocation = pokedexReference.getLocationDex.getLocationList()[0];
    }
    
-   public
+   /**
+   * This method is an accessor that allows other classes to access
+   * the name of the Person
+   * @return Name of the Person
+   */
+   public String getName () {
+      return name;
+   }
+
+   /**
+   * This method is an accessor that allows other classes to access
+   * the age of the Person
+   * @return Age of the Person
+   */
+   public int getAge () {
+      return age;
+   }
+
+   /**
+   * This method is an accessor that allows other classes to access
+   * the ID of the Person
+   * @return ID of the Person
+   */
+   public int getPersonID () {
+      return personID;
+   }
+
+   /**
+   * This method is an accessor that allows other classes to access
+   * the maximum number of Pokemon a player can have
+   * @return The maximum number of Pokemon a player can have
+   */
+   public int getMaxPokemon () {
+      return maxPokemon;
+   }
+
+   /**
+   * This method is an accessor that allows other classes to access
+   * the current number of Pokemon a player has
+   * @return The current number of Pokemon a player has
+   */
+   public int getNumPokemon () {
+      return numPokemon;
+   }
+
+   /**
+   * This method is an accessor that allows other classes to access
+   * the list of Pokemon is the person's team
+   * @return The current list of Pokemon in the person's team
+   */
+   public Pokemon[] getTeamList () {
+      return teamList;
+   }
+
+   /**
+   * This method is an accessor that allows other classes to access
+   * the current location the person is in
+   * @return The current location the person is in
+   */
+   public Location getCurrentLocation () {
+      return currentLocation;
+   }
+
+   /**
+   * This method is an accessor that allows other classes to access
+   * the Pokedex
+   * @return The Pokedex object each person has
+   */
+   public int getPokedexReference () {
+      return pokedexReference;
+   }
+
+   /**
+    * This method gives an item whose name is passed through
+    * the parameter to a Pokemon whose name is passed through
+    * the parameter.
+    * @param poke The name of the Pokemon
+    * @param it The name of the Item
+    * @return True if the item was successfully given and false otherwise
+    */
+   public boolean giveItem (String poke, String it) {
+      for ()
+      Pokemon p_temp = this.searchPokemonByNameInTeam(poke);
+      if (this instanceof NPC) {
+         Item i_temp = pokedexReference.getItemDex.searchItemByName(it);
+         p_temp.setItemHolding(i_temp);
+      } else {
+         Item i_temp = this.searchItemByNameInTeam(it);
+         if (i_temp == null) {
+            return false;
+         } else {
+            p_temp.setItemHolding(i_temp);
+         }
+      }
+   }
 }
