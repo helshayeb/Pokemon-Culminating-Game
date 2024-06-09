9a68342fa7caacff159f4f2941b93d900f45535a
TarantulaHawk512 <david.guan3@student.tdsb.on.ca>
2024-06-09 09:14:33 -0400

    Update DamageMove.java

--- MoveHierarchy/DamageMove.java
+++ MoveHierarchy/DamageMove.java
@@ -7,7 +7,9 @@ public class DamageMove extends Move{
    }
    
    public void useMove(Pokemon p1, Pokemon p2){
-      p2.setHPStat(0.2*(p1.getAtkStat()*basePower)/p2.getDefStat());
+      double multiplier = super.getMultiplier(super.getMoveType(), p2.getType());
+      double damgeDone = -0.2*(p1.getAtkStat()*basePower)/p2.getDefStat()*multiplier;
+      p2.setHPStat(0, damageDone);
    }
    
    public String toString(){
