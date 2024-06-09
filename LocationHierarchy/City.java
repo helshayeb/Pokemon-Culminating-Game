4e669189d3f677d865e84e018cdd6063cc89eaec
gummyBearOnTheBeach <kelvin.kim@student.tdsb.on.ca>
2024-06-09 11:11:38 -0400

    Update City.java

--- LocationHierarchy/City.java
+++ LocationHierarchy/City.java
@@ -1,54 +1,43 @@
 public class City extends Location {
-  private boolean hasGym;
-  private boolean hasPokeCentre;
-  private boolean hasStore;
+    private boolean hasPokeCentre;
+    private boolean hasStore;
 
-  public City(String type, String name, int id, boolean gym, boolean pokeCenter, boolean store){
-    super(type, name, id);
-    hasGym = gym;
-    hasPokeCentre = pokeCenter;
-    hasStore = store;
-  }
-
- public City(int id, boolean gym, boolean pokeCenter, boolean store){
-    super(id);
-    hasGym = gym;
-    hasPokeCentre = pokeCenter;
-    hasStore = store;
- }
-
-public City(int id){
-    super(id);
-    hasGym = false;
-    hasPokeCentre = false;
-    hasStore = false;
-}
-
-
-
-      // Accessors (Getters)
-  public boolean hasGym() {
-      return hasGym;
+    public City(String type, String name, int id, boolean pokeCenter, boolean store){
+        super(type, name, id);
+        hasPokeCentre = pokeCenter;
+        hasStore = store;
     }
 
-   public boolean hasPokeCentre() {
-     return hasPokeCentre;
-   }
-  public boolean hasStore() {
-     return hasStore;
-   }
-    // Mutators (Setters)
-   public void setHasGym(boolean hasGym) {
-     this.hasGym = hasGym;
-   }
+    public City(int id, boolean pokeCenter, boolean store){
+        super(id);
+        hasPokeCentre = pokeCenter;
+        hasStore = store;
+    }
 
-  public void setHasPokeCentre(boolean hasPokeCentre) {
-     this.hasPokeCentre = hasPokeCentre;
- }
-  public void setHasStore(boolean hasStore) {
-     this.hasStore = hasStore;
-  }
-  
+    public City(int id){
+        super(id);
+        hasPokeCentre = false;
+        hasStore = false;
+    }
+
+
+
+    // Accessors (Getters)
+
+    public boolean hasPokeCentre() {
+        return hasPokeCentre;
+    }
+    public boolean hasStore() {
+        return hasStore;
+    }
+    // Mutators (Setters)
+
+    public void setHasPokeCentre(boolean hasPokeCentre) {
+        this.hasPokeCentre = hasPokeCentre;
+    }
+    public void setHasStore(boolean hasStore) {
+        this.hasStore = hasStore;
+    }
+
 }
-  
 
