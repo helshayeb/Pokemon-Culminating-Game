c7cf50192783a25552ce5a63f02df6631fcd86c0
TarantulaHawk512 <david.guan3@student.tdsb.on.ca>
2024-06-10 12:58:14 -0400

    Update MoveDex.java

--- PokeDex/MoveDex.java
+++ PokeDex/MoveDex.java
@@ -3,6 +3,21 @@ public class MoveDex{
   private int numMoveData;
   private Move[] moveList;
   public boolean readMoves(String fileName){
-  
+    try {
+      BufferedReader in = new BufferedReader(new FileReader(fileName));
+      numMoveData = in.readLine();
+      moveList = new Move[numMoveData];
+         
+      for (int i = 0; i < numItemData; i++) {
+        MoveList[i].setMoveTypeName(in.readLine());
+        MoveList[i].setMoveName(in.readLine());
+        MoveList[i].setId(Integer.parseInt(in.readLine()));
+        MoveList[i].setPrice(Double.parseDouble(in.readLine()));
+        MoveList[i].setModifer(Double.parseDouble(in.readLine());
+      }
+      in.close();   
+    }catch (IOException iox) {
+      System.out.println("Error accessing file");
+    }
   }  
 }
