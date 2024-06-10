22e1ddc167822ce937e805b62e9976d59690e3ca
careless8490 <carlos.kau@student.tdsb.on.ca>
2024-06-09 20:10:40 -0400

    y

---
+++ NPC.java
@@ -0,0 +1,12 @@
+public class NPC {
+   /**
+   * This method is a constructor that initializes the fields of
+   * the person when given its name, age, and ID
+   * @param n The name of the person the user inputted
+   * @param a The age of the person the user inputted
+   * @param i The ID of the person, retrieved from the user database
+   */
+   public Person (String n, int a, int i) {
+      super(n, a, i, 0, pokedexReference.getLocationDex.getLocationList()[0]);
+   }
+}
