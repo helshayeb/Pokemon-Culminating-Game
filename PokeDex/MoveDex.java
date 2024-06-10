bc97ef597f0f6086c3ac67186b33ec8e1118bf8c
TarantulaHawk512 <david.guan3@student.tdsb.on.ca>
2024-06-10 13:25:01 -0400

    Update MoveDex.java

--- PokeDex/MoveDex.java
+++ PokeDex/MoveDex.java
@@ -7,12 +7,40 @@ public class MoveDex{
       BufferedReader in = new BufferedReader(new FileReader(fileName));
       numMoveData = in.readLine();
       moveList = new Move[numMoveData];
-      String moveVariation;  
+      String objectID;  
+      String moveName;
+      int moveType;
+      int moveID;
+      int basePower;
+      double hpMod;
+      double atkMod;
+      double defMod;
+      double spdMod;
+      double recipient;
+      double conditionApplied;
       for (int i = 0; i < numItemData; i++) {
-        moveVariation = in.readLine();
-        if(moveVariation.equalsIgnoreCase(Damage){
-          MoveList[i].setMoveTypeName(in.readLine());
-        
+        objectID = in.readLine();
+        if(objectID.equalsIgnoreCase("Damage")){
+          moveName = in.readLine();
+          moveType = Integer.parseInt(in.readLine());
+          moveID = Integer.parseInt(in.readLine());
+          basePower = Integer.parseInt(in.readLine());
+          in.readLine();
+          moveList[i] = new DamageMove(objectID, moveName, moveType, moveID, basePower);
+        }else if(moveVariation.equalsIgnoreCase("Status")){
+          moveName = in.readLine();
+          moveType = Integer.parseInt(in.readLine());
+          moveID = Integer.parseInt(in.readLine());
+          hpMod = Double.parseDouble(in.readLine());
+          atkMod = Double.parseDouble(in.readLine());
+          defMod = Double.parseDouble(in.readLine());
+          spdMod = Double.parseDouble(in.readLine());
+          recipient = Integer.parseInt(in.readLine());
+          in.readLine()
+          moveList[i] = new StatusMove(objectID, moveName, moveType, moveID, hpMod, atkMod, defMod, spdMod, recipient);
+        }else if(moveVariation.equalsIgnoreCase("Condition")){
+          
+        double hpMod, double atkMod, double defMod, double spdMod, int recipient
         MoveList[i].setMoveName(in.readLine());
         MoveList[i].setId(Integer.parseInt(in.readLine()));
         MoveList[i].setPrice(Double.parseDouble(in.readLine()));
