5b1c1ae73ed2830305b75bc1fbdd6f4c29a3164e
gummyBearOnTheBeach <kelvin.kim@student.tdsb.on.ca>
2024-06-10 13:54:11 -0400

    Update PokemonRunner.java

--- PokemonRunner.java
+++ PokemonRunner.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Main{
    public static void main( String[] args){
       System.out.println("Welcome to Pokemons");
-      Scanner sc = new Scanner(System.in);
-      boolean pass = false;
-      while(!pass){
-         System.out.println("1. PokeDex\n2. User Database");
-         System.out.print("Choose An Option: ");
-         String input = sc.nextLine();
-         if(input.equals("1")){
-            pass = true;
-            System.out.println("uwwwwwwuunnnnn");
-         } else if(input.equals("2")){
-            pass = true;
-            System.out.println("tooooooowwwww");
-         } else if(input.equals("0")){
-            System.out.println("Exiting");
-         }else{
-            System.out.println("Invalid Input, Try again");
-         }
+      int first = crossRoad("1. PokeDex\n2. User Database", 2);
+      if(first == 1){
+         int second = crossRoad("1. PokemonDex\n2. LocationDex\n3. ItemDex\n4. MoveDex", 4);
+      } else if(first == 2){
+         
+      } else if(first == 0){
+         
       }
-      
+   }
+   
+   public static int crossRoad(String options, int optionNum){
+      Scanner sc = new Scanner(System.in);
+      System.out.println(options);
+      System.out.print("Choose options: ");
+      boolean valid = false;
+      try{
+         int choice = Integar.parseInt(sc.nextline);
+         if( choice <= optionNum && choice >= 0){
+            return choice;
+         }
+      } catch(Exception x){
+         System.out.println("Invalid Input, Try again");
+         return crossRoad(options, optionNum, exiting);
+      }
    }
 }
