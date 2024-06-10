172079e7de2c43bb834c54ef3c36314b53222048
TarantulaHawk512 <david.guan3@student.tdsb.on.ca>
2024-06-10 13:46:54 -0400

    Update PokeDex.java

--- PokeDex/PokeDex.java
+++ PokeDex/PokeDex.java
@@ -13,7 +13,7 @@ public class PokeDex {
     PokemonDex pmDex;
     LocationDex loDex;
     MoveDex mvDex;
-
+    public final static int MAX_SIZE = 200;
     public PokeDex(ItemDex itDex,PokemonDex pmDex, LocationDex loDex,MoveDex mvDex){
         this.itDex = itDex;
         this.loDex = loDex;
