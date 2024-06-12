14898749111996d4bda4039b49be1d3921042652
TarantulaHawk512 <david.guan3@student.tdsb.on.ca>
2024-06-12 09:25:57 -0400

    Update ConditionMove.java

--- MoveHierarchy/ConditionMove.java
+++ MoveHierarchy/ConditionMove.java
@@ -2,40 +2,53 @@ package MoveHierarchy;
 
 import Pokemon.Pokemon;
 //import ConditionHierarchy.*;
-
+/**
+* Creates Condition Moves which can be used by Pokemon in Battle
+* to apply a condition to the opposing Pokemon
+*/
 public class ConditionMove extends Move{
-   private int conditionAppliedID;
-  // private Condition conditionApplied;
 
-   public int getConditionAppliedID(){
-      return conditionAppliedID;
-   }
-   
-   public ConditionMove(String objectID, String moveName, int moveType, int moveID, int conditionAppliedID){
-      super(objectID, moveName, moveType, moveID);
-      this.conditionAppliedID = conditionAppliedID;
-      /*switch(conditionAppliedID) {
-         case 0:
-            conditionApplied = new Burn();
-            break;
-         case 1:
-            conditionApplied = new Paralysis();
-            break;
-         case 2:
-            conditionApplied = new Poison();
-            break;
-         default:
-            conditionApplied = null;
-            break;
-      }*/
+	/**
+ 	* ID of the Condition that the Move applies
+	*/
+	private int conditionAppliedID;
+ 		// private Condition conditionApplied;
 
-   }
+	
    
-   public void useMove(Pokemon p1, Pokemon p2){
+   	public ConditionMove(String objectID, String moveName, int moveType, int moveID, int conditionAppliedID){
+		super(objectID, moveName, moveType, moveID);
+		this.conditionAppliedID = conditionAppliedID;
+	      /*switch(conditionAppliedID) {
+		 case 0:
+		    conditionApplied = new Burn();
+		    break;
+		 case 1:
+		    conditionApplied = new Paralysis();
+		    break;
+		 case 2:
+		    conditionApplied = new Poison();
+		    break;
+		 default:
+		    conditionApplied = null;
+		    break;
+	      }*/
+
+   	}
+	
+	/**
+ 	* This method is an accessor that allows other classes to access the
+        * ID of the Condition that the Move applies
+	*/
+	public int getConditionAppliedID(){
+    		return conditionAppliedID;
+ 	}
+   
+ 	public void useMove(Pokemon p1, Pokemon p2){
       
-      p2.setCondition(conditionAppliedID);
-   }
-
+		p2.setCondition(conditionAppliedID);
+	}
+   
 	public String toString() {
 		return null;
 	}
