d58756395241b7484ced63be6c182e7e3a5732f5
TarantulaHawk512 <david.guan3@student.tdsb.on.ca>
2024-06-11 23:10:26 -0400

    Update Poison.java

--- ConditionHierarchy/Poison.java
+++ ConditionHierarchy/Poison.java
@@ -1,21 +1,26 @@
-public class Poison extends Condition{
-  public static final double DMG = -0.1;
+package ConditionHierarchy;
 
-  public Poison(Pokemon appliedTo){ //wait appliedTo might have to be a thing in condition
-    super(appliedTo);
-  }
+import Pokemon.*;
 
-  public void apply(){
-    if(super.getTurnsRemaining() != 0){
-      appliedTo.changeCurrentHP(DMG, 0);
-      super.reduceTurns();
-    }else{
-      appliedTo.changeCurrentHP(DMG, 0);
-      appliedTo.setCondition(-1);
-    }
-  }
-   
-  public StringtoString(){
-    return "Poison";
-  }
+public class Poison extends Condition {
+	public static final double DMG = -0.1;
+	public static final int CONDITION_ID = 2;
+
+	public Poison(Pokemon appliedTo) { // wait appliedTo might have to be a thing in condition
+		super(appliedTo, CONDITION_ID);
+	}
+
+	public void apply() {
+		if (super.getTurnsRemaining() != 0) {
+			super.getAppliedTo().changeCurrentHP(DMG, 0);
+			super.reduceTurns();
+		} else {
+			super.getAppliedTo().changeCurrentHP(DMG, 0);
+			super.getAppliedTo().setCondition(-1);
+		}
+	}
+
+	public String toString() {
+		return "Poison";
+	}
 }
