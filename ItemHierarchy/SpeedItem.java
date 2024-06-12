162d9b28ad4f9746472fda1e12f6f45a9e6075a1
helshayeb <helshayeb01@gmail.com>
2024-06-12 13:51:27 -0400

    Update SpeedItem.java

--- ItemHierarchy/SpeedItem.java
+++ ItemHierarchy/SpeedItem.java
@@ -1,5 +1,9 @@
 public class SpeedItem extends Item {
 
+public SpeedItem(String type, String name, int idNum, int priceNum, double mod) {
+    super(type, name, idNum, priceNum, mod);
+  }
+  
 public void useItem(Pokemon p) {
   p.changeSpeedStat(this.modifier); //Multiplies the pokemon's speed stat by the item's modifier
 }
