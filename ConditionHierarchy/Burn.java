caeb98a539fdcbfb624372007803a5931b16b2ac
TarantulaHawk512 <david.guan3@student.tdsb.on.ca>
2024-06-11 23:09:24 -0400

    Update Burn.java

--- ConditionHierarchy/Burn.java
+++ ConditionHierarchy/Burn.java
@@ -1,23 +1,27 @@
+package ConditionHierarchy;
+
+import Pokemon.*;
 public class Burn extends Condition{
   public static final double ATK_MOD = 0.5;
   public static final double DMG = -0.05;
+  public static final int CONDITION_ID = 0;
   private double originalAtk;
 
   public Burn(Pokemon appliedTo){ //wait appliedTo might have to be a thing in condition
-    super(appliedTo);
-    originalAtk = appliedTo.getAtkStat();
+    super(appliedTo, CONDITION_ID);
+    originalAtk = appliedTo.getAttackStat();
   }
 
   public void apply(){
     if(super.getTurnsRemaining() == 4){
-      appliedTo.changeAtkStat(ATK_MOD);
-      appliedTo.changeCurrentHP(DMG, 0);
+    	super.getAppliedTo().changeAtkStat(ATK_MOD);
+    	super.getAppliedTo().changeCurrentHP(DMG, 0);
       super.reduceTurns();
     }else if (super.getTurnsRemaining() == 0){
-      appliedTo.setAtkStat(originalAtk);
-      appliedTo.setCondition(-1);
+    	super.getAppliedTo().setAttackStat(originalAtk);
+    	super.getAppliedTo().setCondition(-1);
     }else{
-      appliedTo.changeCurrentHP(DMG, 0);
+    	super.getAppliedTo().changeCurrentHP(DMG, 0);
       super.reduceTurns();
     }
   }
