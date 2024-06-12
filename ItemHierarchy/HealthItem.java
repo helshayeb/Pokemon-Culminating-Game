9ea5d67880b20d242362698926b26d1693bf3537
helshayeb <helshayeb01@gmail.com>
2024-06-12 13:50:23 -0400

    Update HealthItem.java

--- ItemHierarchy/HealthItem.java
+++ ItemHierarchy/HealthItem.java
@@ -1,13 +1,15 @@
 public class HealthItem extends Item {
 
+public HealthItem(String type, String name, int idNum, int priceNum, double mod) {
+    super(type, name, idNum, priceNum, mod);
+  }
+  
 public void useItem(Pokemon p) {
-  //checks if the pokemon's hp will overflow after taking the item
-  if (p.getCurrentHp + modifier <= p.getMaxHpStat) {
-    p.changeCurrentHp(0, this.modifier); //heals the pokemon 
+  if (p.getCurrentHp() + modifier <= p.getMaxHpStat()) {
+    p.changeCurrentHp(0, this.modifier);
 }
   else {
-    p.changeCurrentHp(0, this.modifier); //brings the pokemon to full health instead of overflowing
+    p.changeCurrentHp(0, this.modifier); 
   }
 }
-
 }
