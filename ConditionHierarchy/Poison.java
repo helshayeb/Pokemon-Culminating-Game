afa5660c73f5f6b81ca2139ab14272f93fb6c38c
TarantulaHawk512 <david.guan3@student.tdsb.on.ca>
2024-06-12 08:05:40 -0400

    Update Poison.java

--- ConditionHierarchy/Poison.java
+++ ConditionHierarchy/Poison.java
@@ -1,15 +1,36 @@
 package ConditionHierarchy;
 
 import Pokemon.*;
-
+/**
+* This class creates and allows the Poison Condition to be used and applied to Pokemon 
+* in the Pokemon game
+* The Poison Condition deals considerable damage to the Pokemon every turn it is applied on
+*/
 public class Poison extends Condition {
+
+	/**
+        * Damage (represented as a portion of max hp) done to Poisoned Pokemon every turn
+        */
 	public static final double DMG = -0.1;
+
+	/**
+        * Condition ID of the Poison Condition, 2
+        */
 	public static final int CONDITION_ID = 2;
 
-	public Poison(Pokemon appliedTo) { // wait appliedTo might have to be a thing in condition
+	/**
+	* This method is a constructor that initializes the fields of
+	* the Poison class when given its 
+	* @param appliedTo Pokemon the Condition is applied to
+ 	*/
+	public Poison(Pokemon appliedTo) { 
 		super(appliedTo, CONDITION_ID);
 	}
 
+	/**
+        * This method applies the turn-based effects of the condition onto the
+        * the Pokemon that the Poison is applied to
+        */
 	public void apply() {
 		if (super.getTurnsRemaining() != 0) {
 			super.getAppliedTo().changeCurrentHP(DMG, 0);
