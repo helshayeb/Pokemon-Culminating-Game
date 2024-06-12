b5dc16ccd7998c3338139c242cbb68c8e1af7050
helshayeb <helshayeb01@gmail.com>
2024-06-12 13:50:37 -0400

    Update DefenceItem.java

--- ItemHierarchy/DefenceItem.java
+++ ItemHierarchy/DefenceItem.java
@@ -1,5 +1,9 @@
 public class DefenceItem extends Item {
 
+public DefenceItem(String type, String name, int idNum, int priceNum, double mod) {
+    super(type, name, idNum, priceNum, mod);
+  }
+  
 public void useItem(Pokemon p) {
   p.changeDefenceStat(this.modifier); //Multiplies the pokemon's defence stat by the item's modifier
 }
