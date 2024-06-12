81ab1706d6a66721f21b90a982bc0327a047386a
gummyBearOnTheBeach <kelvin.kim@student.tdsb.on.ca>
2024-06-11 21:53:19 -0400

    Update LocationDex.java

--- PokeDex/LocationDex.java
+++ PokeDex/LocationDex.java
@@ -1,8 +1,10 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class LocationDex {
-    ArrayList<Location> locDex;
+    int numData;
+    int MAX_SIZE = 10;
+    Location[] locDex;
 
     /**
      *
@@ -14,11 +16,10 @@ public class LocationDex {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
             for (int i = 0; i < Integer.parseInt(br.readLine()); i++) {
 
-
                 String type = br.readLine().toLowerCase();
                 if (type.equals("city") || type.equals("c")) {
-                    locDex.add(new City(br.readLine(), br.readLine(), Integer.parseInt(br.readLine()),Boolean.parseBoolean(br.readLine()),Boolean.parseBoolean(br.readLine())));
-
+                    locDex[i] = new City(br.readLine(), br.readLine(), Integer.parseInt(br.readLine()),Boolean.parseBoolean(br.readLine()),Boolean.parseBoolean(br.readLine()));
+                    numData++;
                 } else if(type.equals("route")||type.equals("r")){
                     String region = br.readLine();
                     String name = br.readLine();
@@ -26,37 +27,81 @@ public class LocationDex {
                     int routeNum = Integer.parseInt(br.readLine());
                     br.readLine();
                     int[] list= {Integer.parseInt(br.readLine()),Integer.parseInt(br.readLine()),Integer.parseInt(br.readLine())};
-                    locDex.add(new Route(region, name, id, routeNum, list));
+                    locDex[i] = new Route(region, name, id, routeNum, list);
+                    numData++;
                 } else {
                     return false;
                 }
+                br.readLine();
             }
             return true;
         } catch(IOException iox){
             System.out.println("File Not Found");
             return false;
         } catch(ArrayIndexOutOfBoundsException aioobx){
-            System.out.println("Wrong Number of Elements");
+            System.out.println("Location is full");
             return false;
         }
     }
 
+    public Location searchLocationByID(int id){
+        int left = 0;
+        int right = numData-1;
+        searchLocationByID(locDex, id, left, right);
+    }
+
+    public int searchLocationByID(Location[] locList, int id, int left, int right){
+        int mid = left+(right-left)/2;
+        if(left>right){
+            return -1;
+        }
+        if(locList[mid].getLoactionID() == id){
+            return mid;
+        }else if(locList[mid].getLoactionID() > id){
+        return searchLocationByID(locList, id, left, mid-1);
+    }else{
+        return searchLocationByID(locList, id, mid+1, right);
+    }
+}
+
+    public int searchLocationByName(String name){
+        for(int i = 0; i < numData; i++){
+            if(locDex[i].getName().equalsIgnoreCase(name)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 //   missing search by id & name
 
+    /**
+     *
+     * @param type
+     * @param name
+     * @param id
+     * @param pokeCenter
+     * @param store
+     */
     public void addCity(String type, String name, int id, boolean pokeCenter, boolean store){
-        locDex.add(new City(type, name, id,pokeCenter, store));
+        locDex[numData] = new City(type, name, id,pokeCenter, store);
+        numData++;
     }
 
     public void addCity(int id, boolean pokeCenter, boolean store){
-        locDex.add(new City(id, pokeCenter,store));
+        locDex[numData] = new City(id, pokeCenter,store);
+        numData++;
     }
 
     public void addRoute(String type, String name, int routeNum, int id,int[] pokes){
-        locDex.add(new Route(type,name,routeNum,id,pokes));
+        locDex[numData] = new Route(type,name,routeNum,id,pokes);
+        numData++;
     }
 
     public void addRoute(String type, String name, int routeNum, int id){
-        locDex.add(new Route(type, name, routeNum, id));
+        locDex[numData] = new Route(type, name, routeNum, id);
+        numData++;
     }
 
     /**
@@ -69,18 +114,19 @@ public class LocationDex {
             File file = new File(fileName);
             file.createNewFile();
             BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
-            bw.write(locDex.size()); /*Number of Location*/
-            for (int i = 0; i < locDex.size(); i++) {
-                if(locDex.get(i) instanceof City)/*If it is city or not*/{
-                    City temp = (City)(locDex.get(i));
+            bw.write(locDex.length); /*Number of Location*/
+            for (int i = 0; i < locDex.length; i++) {
+                if(locDex[i] instanceof City)/*If it is city or not*/{
+                    City temp = (City)(locDex[i];
                     bw.write("City\n"+ temp.getRegionType()+"\n"+ temp.getName()+"\n"+ temp.getLoactionID() +"\n"+temp.getHasStore() +"\n"+temp.getHasPokeCentre()+"\n");
-                } else if(locDex.get(i) instanceof Route)/*If it is route or not*/{
-                    Route tempp = (Route)locDex.get(i);
+                } else if(locDex[i] instanceof Route)/*If it is route or not*/{
+                    Route tempp = (Route)locDex[i];
                     bw.write("Route\n" +"\n"+tempp.getRegionType()+"\n"+ tempp.getName()+"\n"+ tempp.getLoactionID() + "\n"+ tempp.getRouteNum() + "\n"+tempp.getNumPokemon());
                     for (int j = 0; j < tempp.getNumPokemon(); j++) {
                         bw.write(tempp.getPokemonInLocation()[i]+"\n");
                     }
                 }
+                bw.write("\n");
             }
             return true;
         }catch(IOException iox){
@@ -96,4 +142,3 @@ public class LocationDex {
         return locDex.toString();
     }
 }
-
