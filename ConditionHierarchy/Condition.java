394ab8ae7223ab7270cc5873bbe4a37899072c4a
TarantulaHawk512 <david.guan3@student.tdsb.on.ca>
2024-06-12 07:50:55 -0400

    Update Condition.java

--- ConditionHierarchy/Condition.java
+++ ConditionHierarchy/Condition.java
@@ -1,29 +1,56 @@
 package ConditionHierarchy;
 
 import Pokemon.Pokemon;
+/**
+* This class creates the Condition abstract class, which allows other Conditions to be used 
+* in the Pokemon game
+*/
 public abstract class Condition{
-   private int conditionID;
-   private int turnsRemaining;
-   private Pokemon appliedTo;
-   
-   public Condition(Pokemon appliedTo, int conditionID) {
+
+   	/**
+ 	* Condition ID of the Condition
+	*/
+	private int conditionID;
+
+	/**
+ 	* Turns remaining until the Condition wears off
+	*/
+	private int turnsRemaining;
+
+	/**
+ 	* Pokemon that the Condition is applied to
+	*/
+	private Pokemon appliedTo;
+
+	/**
+	* This method is a constructor that initializes the fields of
+	* the Condition class when given its 
+	* @param appliedTo Pokemon the Condition is applied to
+	* @param conditionID the ID of the condition, to be specified in
+ 	* the child classes
+ 	*/
+	public Condition(Pokemon appliedTo, int conditionID) {
 	  this.appliedTo = appliedTo;
 	  this.conditionID = conditionID;
-   }
+	}
+	
+	public int getConditionID(){
+	return conditionID;
+	}
+	public int getTurnsRemaining(){
+	return turnsRemaining;
+	}
+	public Pokemon getAppliedTo(){
+	return appliedTo;
+	}
+	public void reduceTurns(){
+	turnsRemaining--;
+	}
 
-   public int getConditionID(){
-      return conditionID;
-   }
-   public int getTurnsRemaining(){
-      return turnsRemaining;
-   }
-   public Pokemon getAppliedTo(){
-      return appliedTo;
-   }
-   public void reduceTurns(){
-      turnsRemaining--;
-   }
-
-   public abstract void apply();
+	/**
+ 	* This abstract method will apply the turn-based effects of the Condition onto the
+ 	* the Pokemon that the specified Condition is applied to
+	*/
+	public abstract void apply();
 
 }
