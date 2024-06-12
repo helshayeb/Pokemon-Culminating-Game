f53234a3ca9c9a202c1ef1c7c6d01e3b0cf9baf1
TarantulaHawk512 <david.guan3@student.tdsb.on.ca>
2024-06-11 23:23:48 -0400

    Update Move.java

--- MoveHierarchy/Move.java
+++ MoveHierarchy/Move.java
@@ -8,7 +8,6 @@ public abstract class Move {
 	private String moveName;
 	private int moveType;
 	private int moveID;
-	MoveDex pd = new MoveDex();
 
 	public Move(String objectID, String moveName, int moveType, int moveID) {
 		this.objectID = objectID;
