e9b163a9900243c1cf39831665b10afc12214ec1
helshayeb <helshayeb01@gmail.com>
2024-06-13 01:01:48 -0400

    Update SpeedItem.java

--- ItemHierarchy/SpeedItem.java
+++ ItemHierarchy/SpeedItem.java
@@ -1,28 +1,10 @@
-/**
- * Class representing a speed item that can be used to modify a Pokemon's speed stat.
- */
 public class SpeedItem extends Item {
 
-  /**
-   * Constructs a SpeedItem with the specified type, name, ID, price, and modifier.
-   *
-   * @param type the type name of the item
-   * @param name the name of the item
-   * @param idNum the ID number of the item
-   * @param priceNum the price of the item
-   * @param mod the modifier value of the item
-   */
-  public SpeedItem(String type, String name, int idNum, int priceNum, double mod) {
+public SpeedItem(String type, String name, int idNum, int priceNum, double mod) {
     super(type, name, idNum, priceNum, mod);
   }
 
-  /**
-   * Uses the speed item on the specified Pokemon to modify its speed stat.
-   *
-   * @param p the Pokemon to use the speed item on
-   */
-  @Override
-  public void useItem(Pokemon p) {
-    p.changeSpeedStat(getModifier()); // Multiplies the Pokemon's speed stat by the item's modifier
-  }
+public void useItem(Pokemon p) {
+  p.changeSpeedStat(this.modifier); //Multiplies the pokemon's speed stat by the item's modifier
+}
 }
