834360f218605e27a135ecf5ff4d38a1f357b1d6
helshayeb <helshayeb01@gmail.com>
2024-06-13 00:33:39 -0400

    Update DefenceItem.java

--- ItemHierarchy/DefenceItem.java
+++ ItemHierarchy/DefenceItem.java
@@ -1,10 +1,28 @@
+/**
+ * Class representing a defence item that can be used to modify a Pokemon's defence stat.
+ */
 public class DefenceItem extends Item {
 
-public DefenceItem(String type, String name, int idNum, int priceNum, double mod) {
+  /**
+   * Constructs a DefenceItem with the specified type, name, ID, price, and modifier.
+   *
+   * @param type the type name of the item
+   * @param name the name of the item
+   * @param idNum the ID number of the item
+   * @param priceNum the price of the item
+   * @param mod the modifier value of the item
+   */
+  public DefenceItem(String type, String name, int idNum, int priceNum, double mod) {
     super(type, name, idNum, priceNum, mod);
   }
-  
-public void useItem(Pokemon p) {
-  p.changeDefenceStat(this.modifier); //Multiplies the pokemon's defence stat by the item's modifier
+
+  /**
+   * Uses the defence item on the specified Pokemon to modify its defence stat.
+   *
+   * @param p the Pokemon to use the defence item on
+   */
+  @Override
+  public void useItem(Pokemon p) {
+    p.changeDefenceStat(getModifier()); // Multiplies the Pokemon's defence stat by the item's modifier
+  }
 }
-} 
