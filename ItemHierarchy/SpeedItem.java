ba64af9e82966ed6aad794bc4a89d0a96c48a19d
helshayeb <helshayeb01@gmail.com>
2024-06-13 01:28:15 -0400

    Update SpeedItem.java

--- ItemHierarchy/SpeedItem.java
+++ ItemHierarchy/SpeedItem.java
@@ -1,10 +1,26 @@
+/**
+* Creates speed items that can increase a pokemon's defence stat
+*/
 public class SpeedItem extends Item {
 
-public SpeedItem(String type, String name, int idNum, int priceNum, double mod) {
-    super(type, name, idNum, priceNum, mod);
-  }
+   /**
+    * This method is a constructor that initializes the fields of
+    * the speed item when given its type, name, id, price, and modifier
+    * @param type The type of item this item falls under, retrieved from the ItemDex
+    * @param name The name of the item, retrieved from the ItemDex
+    * @param idNum The ID of the item, retrieved from the ItemDex
+    * @param priceNum The price of the item, retrieved from the ItemDex
+    * @param mod The modifier of the item, retrieved from the ItemDex
+    */ 
+    public SpeedItem(String type, String name, int idNum, int priceNum, double mod) {
+        super(type, name, idNum, priceNum, mod);
+      }
 
-public void useItem(Pokemon p) {
-  p.changeSpeedStat(this.modifier); //Multiplies the pokemon's speed stat by the item's modifier
-}
+     /**
+    * Allows a pokemon to use a speed item
+    * @param p The pokemon the speed item's effect will be applied to
+    */
+    public void useItem(Pokemon p) {
+      p.changeSpeedStat(this.modifier); //Multiplies the pokemon's speed stat by the item's modifier
+    }
 }
