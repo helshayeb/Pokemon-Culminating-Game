ad51d161f46a65152bd662126cb5d5698feeed90
helshayeb <helshayeb01@gmail.com>
2024-06-13 00:33:14 -0400

    Update AttackItem.java

--- ItemHierarchy/AttackItem.java
+++ ItemHierarchy/AttackItem.java
@@ -1,10 +1,28 @@
+/**
+ * Class representing an attack item that can be used to modify a Pokemon's attack stat.
+ */
 public class AttackItem extends Item {
 
-public AttackItem(String type, String name, int idNum, int priceNum, double mod) {
+  /**
+   * Constructs an AttackItem with the specified type, name, ID, price, and modifier.
+   *
+   * @param type the type name of the item
+   * @param name the name of the item
+   * @param idNum the ID number of the item
+   * @param priceNum the price of the item
+   * @param mod the modifier value of the item
+   */
+  public AttackItem(String type, String name, int idNum, int priceNum, double mod) {
     super(type, name, idNum, priceNum, mod);
   }
-  
-public void useItem(Pokemon p) {
-  p.changeAttackStat(this.modifier); //Multiplies the pokemon's attack stat by the item's modifier
+
+  /**
+   * Uses the attack item on the specified Pokemon to modify its attack stat.
+   *
+   * @param p the Pokemon to use the attack item on
+   */
+  @Override
+  public void useItem(Pokemon p) {
+    p.changeAttackStat(getModifier()); // Multiplies the Pokemon's attack stat by the item's modifier
+  }
 }
-}  
