dde5f8af90a5417a82f4a5d7e4d446ed1fbb4a74
careless8490 <carlos.kau@student.tdsb.on.ca>
2024-06-15 16:59:12 -0400

    Update UserDatabase.java

--- UserDatabase.java
+++ UserDatabase.java
@@ -340,13 +340,15 @@ public class UserDatabase {
 
             for (int j = 0; j < numPoke; j++) {
                 itemID = Integer.parseInt(in.readLine());
-                Item it = null;
-                for (int k = 0; k < pD.getItemDex().getNumItemData(); k++) {
-                	if (itemID == pD.getItemDex().getItemList()[k].getId()) {
-                		it = pD.getItemDex().getItemList()[k];
-                	}
+                if (itemID != -1) {
+                  Item it = null;
+                  for (int k = 0; k < pD.getItemDex().getNumItemData(); k++) {
+                	   if (itemID == pD.getItemDex().getItemList()[k].getId()) {
+                		   it = pD.getItemDex().getItemList()[k];
+                	   }
+                  }
+                  npcList[i].giveItem(team[j].getName(), it.getItemName());
                 }
-                npcList[i].giveItem(team[j].getName(), it.getItemName());
             }
          }
          in.close();
