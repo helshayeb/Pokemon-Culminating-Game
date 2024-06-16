2b17d13b9a716b501afb3d266a42fa96f2ea39c9
careless8490 <carlos.kau@student.tdsb.on.ca>
2024-06-16 01:29:35 -0400

    Update Pokemon.java

--- Pokemon/Pokemon.java
+++ Pokemon/Pokemon.java
@@ -373,7 +373,7 @@ public class Pokemon{
       }else{
 	      cond = currentCondition.toString();
       }
-      String pokemonInfo = "Name: " + name + "\nType: " + getTypeName(type) + "\nID: " + id + "\nMax Health: " + maxHPStat + "\nAttack Stat: " + attackStat + "\nDefence Stat" + defenceStat + "\nSpeedStat: " + speedStat + "Moves: " + moveList[0].getMoveName() + "\n" + moveList[1].getMoveName() + "\n" + moveList[2].getMoveName() + "\n" + moveList[3].getMoveName() + "\nCondition: " + cond + "\nLocation: " + foundIn.getRegionType();  
+      String pokemonInfo = "Name: " + name + "\nType: " + getTypeName(type) + "\nID: " + id + "\nMax Health: " + maxHPStat + "\nAttack Stat: " + attackStat + "\nDefence Stat: " + defenceStat + "\nSpeedStat: " + speedStat + "\nMoves: " + moveList[0].getMoveName() + "\n" + moveList[1].getMoveName() + "\n" + moveList[2].getMoveName() + "\n" + moveList[3].getMoveName() + "\nCondition: " + cond + "\nLocation: " + foundIn.getRegionType();  
       return pokemonInfo;
    }
 }
